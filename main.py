8143af9f911cd50fb6c74b3485c20a0a0ed99d14
davibcs <davibezerracs@gmail.com>
2024-08-22 09:34:01 -0300

    Davi

---
+++ main.py
@@ -0,0 +1,21 @@
+
+nome = input('Escreva o seu nome completo: ')
+email = input('Escreva o seu email: ')
+cpf = int(input('Digite o seu cpf: '))
+telefone = int(input('Digite o seu número de telefone: '))
+
+alcool_mensal = 64 * 20 / 14
+gasolina_mensal = 64 * 20 / 12
+
+print("\n")
+print("------------")
+print("O seu nome completo é",nome)
+print("O seu email é:",email)
+print("O seu cpf é:",cpf)
+print("O seu telefone é:",telefone)
+print("••••••••••••")
+print(f"O uso mensal de alcool é:{alcool_mensal}litros")
+print(f"O uso mensal de gasolina é:{gasolina_mensal}litros")
+print("------------")
+print("\n")
+
